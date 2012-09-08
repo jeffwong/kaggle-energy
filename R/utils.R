096bde4860b4d90918f8c1f47f39cc9f1c246d6d
Jeffrey Wong <jeff.ct.wong@gmail.com>
2012-09-08 14:14:02 -0700

    Template for transformation function using fastVAR format

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,16 @@
+require(plyr)
+
 ##################
 #Utility functions
 ##################
 
 #Transform raw data into fastVAR format
-transform.fastVAR = function(data) {
-
+transform.fastVAR = function(rawLoadData, rawTempData) {
+    load = data.frame(dlply(rawLoadData, .variables="zone_id", .fun = function(zone) {
+        as.vector(t(zone[,-(1:4)]))
+    }))
+    temp = data.frame(dlply(rawTempData, .variables="station_id", .fun = function(station) {
+        as.vector(t(station[,-(1:4)]))
+    }))
+    return (list(load, temp))
 }
